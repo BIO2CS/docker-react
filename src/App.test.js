6e0790bada162529d7506e000edb7233933beeac
Huiqiong Gu <hgu@buffalo.edu>
2024-01-18 11:11:42 -0800

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn frontend and backend/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn fullstack link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learn Frontend and Backend/i);
   expect(linkElement).toBeInTheDocument();
 });
